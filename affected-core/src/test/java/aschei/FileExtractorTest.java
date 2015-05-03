37b709c9d7273472f5d3a519801086020751c7b7
Albrecht Scheidig <albrecht.scheidig@hype.de>
2015-05-03 21:35:51 +0200

    More stuff

---
+++ affected-core/src/test/java/aschei/FileExtractorTest.java
@@ -0,0 +1,15 @@
+package aschei;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class FileExtractorTest {
+
+	@Test
+	public void test() throws IOException {
+		FileExtractor.main(new String[]{"target/test-classes/example.diff"});
+	}
+
+}
